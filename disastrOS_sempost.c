cbb091017dbee1a9482ec2f28a52effa5a797938
flaviamas <flavia_ma@hotmail.it>
2019-08-24 16:15:00 +0200

    error fixed

--- disastrOS_sempost.c
+++ disastrOS_sempost.c
@@ -10,14 +10,23 @@ void internal_semPost(){
   // do stuff :)
       int fd = running->syscall_args[0];
     SemDescriptor * desc = SemDescriptorList_byFd(&running->sem_descriptors,fd);
-
-    Semaphore * sem = desc->semaphore;
-
-
-    if(sem->count < 0) {
-
+    if( desc == NULL){
+        perror("Error, semaphore not owned by the Application");
+        running->syscall_retvalue = DSOS_ERESOURCENOFD;
+        return;
     }
+    Semaphore * sem = desc->semaphore;
+    SemDescriptorPtr * ptr;
     sem->count+=1;
+    if(sem->count <= 0) {
+        List_insert(&ready_list,ready_list.last,(ListItem*)running);
+       ptr = (SemDescriptorPtr*)List_detach(&sem->waiting_descriptors,sem->waiting_descriptors.first);
+       List_insert (&sem->descriptors, sem->descriptors.last,(ListItem*)ptr);
+       List_detach(&waiting_list, (ListItem*) ptr->descriptor->pcb);
+       running->status = Ready;
+       running = ptr->descriptor->pcb;
+    }
+
     running->syscall_retvalue = 0;
     return;
 
--- disastrOS_semwait.c
+++ disastrOS_semwait.c
@@ -10,19 +10,20 @@ void internal_semWait(){
     int fd = running->syscall_args[0];
     SemDescriptor * desc = SemDescriptorList_byFd(&running->sem_descriptors,fd);
     if (desc == NULL){
-    running->syscall_retvalue = DSOS_ERESOURCENOFD ;
+    running->syscall_retvalue = DSOS_ERESOURCENOFD;
     perror("Error, semaphore not owned by the Application");
     return;
     }
     Semaphore * sem = desc->semaphore;
     SemDescriptorPtr * ptr = desc->ptr;
-    PCB * process;
+
     sem->count-=1;
     if(sem->count < 0) {
         List_detach(&sem->descriptors,(ListItem*) ptr);
         List_insert(&sem->waiting_descriptors,sem->waiting_descriptors.last,(ListItem*) ptr);
         running->status = Waiting;
-        List_insert(&waiting_list, waiting_list.last,(ListItem*) runnning);
+        PCB * process;
+        List_insert(&waiting_list, waiting_list.last,(ListItem*) running);
         process = (PCB*)List_detach(&ready_list,(ListItem*)ready_list.first);
     }
     running->syscall_retvalue = 0;
